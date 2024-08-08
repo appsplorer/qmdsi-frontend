8045b2fbc07ade691362d801e6f9287ee4e9c963
asherafridi <ashirchannel765@gmail.com>
2024-08-08 14:12:16 +0500

    revsiosn completed

--- src/components/TransactionModal.jsx
+++ src/components/TransactionModal.jsx
@@ -1,14 +1,27 @@
 import { CircleDivideIcon, X } from 'lucide-react';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Loader from './Loader';
 
 const TransactionModal = ({ closeModal,transactionComplete }) => {
     const [tab, setTab] = useState('Details'); // Correctly defining the state and setState function
     
+    const [loading, setLoading] = useState(false);
 
+    const handleTransaction = ()=>{
+
+        setLoading(true);
+        // Simulating a network request or transaction delay
+        setTimeout(() => {
+            setLoading(false);
+            
+        transactionComplete();
+        }, 2000);
+    }
 
     return (
         <div className='fixed inset-0 z-50 flex items-center justify-center'>
+            {loading && <Loader />} {/* Show spinner when loading */}
             <div className='modal-wrapper fixed inset-0 bg-black opacity-70'></div>
             <div className='w-full md:w-1/3 lg:w-1/4 bg-accent  z-10 rounded-md shadow-lg flex relative overflow-hidden'>
                 <div className='w-full h-full bg-accent p-4'>
@@ -19,11 +32,15 @@ const TransactionModal = ({ closeModal,transactionComplete }) => {
                         </button>
                     </div>
                     <div className='w-full gap-3 mt-4'>
-                        <div className='flex flex-col justify-around items-center w-full text-white mt-12'>
+                        <div className='flex flex-col justify-around items-center w-full text-white mt-4'>
                             <h1 className='text-3xl font-medium'>100 USDT</h1>
-                            <p className='text-sm text-gray-400'>Balance: $99.43</p>
+                            {/* <p className='text-sm text-gray-400'>Balance: $99.43</p> */}
                         </div>
-                        <div className='flex text-white w-full justify-center gap-4 text-xl font-thin mt-24'>
+                        <div className='flex flex-col justify-around items-center w-full text-white '>
+                        <p className='text-sm text-gray-400 text-yellow-300'>to</p>
+                            <h1 className='text-3xl font-medium'>99 QMGT</h1>
+                        </div>
+                        <div className='flex text-white w-full justify-center gap-4 text-xl font-thin mt-6'>
                             <button
                                 className={tab === 'Details' ? 'font-bold border-b-2 border-white' : ''}
                                 onClick={() => setTab('Details')}
@@ -76,7 +93,7 @@ const TransactionModal = ({ closeModal,transactionComplete }) => {
                         )}
                         <div className='w-full gap-4 flex mt-12'>
                             <button className='w-1/2 h-[50px] border rounded-md border-primary text-primary mb-2 hover:bg-secondary hover:text-black'  onClick={closeModal}>Reject</button>
-                            <button className='w-1/2 h-[50px] border rounded-md border-primary bg-primary text-black hover:bg-secondary' onClick={transactionComplete}>Confirm</button>
+                            <button className='w-1/2 h-[50px] border rounded-md border-primary bg-primary text-black hover:bg-secondary' onClick={handleTransaction}>Confirm</button>
                         </div>
                     </div>
                 </div>
