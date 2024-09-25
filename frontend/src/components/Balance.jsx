e92fd5304c2ad2f32906a08945fc7c7b5afd96a3
eminisolomon <realsolomon@outlook.com>
2024-09-25 08:19:47 +0100

    change spelling

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -6,35 +6,35 @@ import { getUserBalances } from "../services/users.service";
 import { TOKENAddress, USDTAddress } from "../addresses";
 import { formatEther } from "ethers";
 const balances = [
-    {
-      name: "Bitcoin",
-      symbol: "BTC",
-      value: 8816.2,
-      change: -8.91,
-      color: "#F7931A",
-    },
-    {
-      name: "Bitcoin Cash",
-      symbol: "BCH",
-      value: 320.17,
-      change: -8.2,
-      color: "#8DC351",
-    },
-    {
-      name: "Ethereum",
-      symbol: "ETH",
-      value: 229.22,
-      change: -8.25,
-      color: "#627EEA",
-    },
-    { name: "XRP", symbol: "XRP", value: 0.23, change: -9.0, color: "#23292F" },
-  ];
+  {
+    name: "Bitcoin",
+    symbol: "BTC",
+    value: 8816.2,
+    change: -8.91,
+    color: "#F7931A",
+  },
+  {
+    name: "Bitcoin Cash",
+    symbol: "BCH",
+    value: 320.17,
+    change: -8.2,
+    color: "#8DC351",
+  },
+  {
+    name: "Ethereum",
+    symbol: "ETH",
+    value: 229.22,
+    change: -8.25,
+    color: "#627EEA",
+  },
+  { name: "XRP", symbol: "XRP", value: 0.23, change: -9.0, color: "#23292F" },
+];
 
 const Balance = ({ isOpen, onClose }) => {
   const modalRef = useRef(null);
   const { profile } = useContext(AuthContext);
-  const [tokenBalance, setTokenBalance] = useState("0")
-  const [usdtBalance, setUsdtBalance] = useState("0")
+  const [tokenBalance, setTokenBalance] = useState("0");
+  const [usdtBalance, setUsdtBalance] = useState("0");
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -53,22 +53,19 @@ const Balance = ({ isOpen, onClose }) => {
   }, [isOpen, onClose]);
 
   useEffect(() => {
-    if(!profile) return 
+    if (!profile) return;
 
-    getTokenBalance(TOKENAddress, profile.walletAddress).then((tokenBalance) => {
-      setTokenBalance(formatEther(tokenBalance))
-    })
+    getTokenBalance(TOKENAddress, profile.walletAddress).then(
+      (tokenBalance) => {
+        setTokenBalance(formatEther(tokenBalance));
+      }
+    );
     getTokenBalance(USDTAddress, profile.walletAddress).then((tokenBalance) => {
-      setUsdtBalance(formatEther(tokenBalance))
-    })
-
-  }, [profile])
-
-
+      setUsdtBalance(formatEther(tokenBalance));
+    });
+  }, [profile]);
 
   if (!isOpen) return null;
-
-  
 
   const totalValue = balances.reduce((sum, coin) => sum + coin.value, 0);
 
@@ -90,50 +87,41 @@ const Balance = ({ isOpen, onClose }) => {
           </div> */}
           <div className="text-red-400 text-sm mb-4"></div>
           <div className="space-y-2">
-
-            
-              <div
-                
-                className="flex justify-between items-center"
-              >
-                <div className="flex items-center space-x-2">
-                  <div
-                    className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold"
-                    style={{ backgroundColor: "#627EEA" }}
-                  >
-                    Q
-                  </div>
-                  <span>QMDT</span>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-2">
+                <div
+                  className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold"
+                  style={{ backgroundColor: "#627EEA" }}
+                >
+                  Q
                 </div>
-                <div className="text-right">
-                  <div>{tokenBalance}</div>
-                  {/* <div className="text-red-400 text-xs">
+                <span>QMGT</span>
+              </div>
+              <div className="text-right">
+                <div>{tokenBalance}</div>
+                {/* <div className="text-red-400 text-xs">
                     {coin.change.toFixed(2)}%
                   </div> */}
-                </div>
               </div>
+            </div>
 
-              <div
-                
-                className="flex justify-between items-center"
-              >
-                <div className="flex items-center space-x-2">
-                  <div
-                    className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold"
-                    style={{ backgroundColor: "#23292F" }}
-                  >
-                    U
-                  </div>
-                  <span>USDT</span>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-2">
+                <div
+                  className="w-6 h-6 rounded-full flex items-center justify-center text-white text-xs font-bold"
+                  style={{ backgroundColor: "#23292F" }}
+                >
+                  U
                 </div>
-                <div className="text-right">
-                  <div>{usdtBalance}</div>
-                  {/* <div className="text-red-400 text-xs">
+                <span>USDT</span>
+              </div>
+              <div className="text-right">
+                <div>{usdtBalance}</div>
+                {/* <div className="text-red-400 text-xs">
                     {coin.change.toFixed(2)}%
                   </div> */}
-                </div>
               </div>
-            
+            </div>
           </div>
         </div>
       </div>
